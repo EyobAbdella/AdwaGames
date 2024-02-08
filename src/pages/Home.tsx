e8ee669668be58f2bb1d3f0715860fa37944f62c
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-02-08 13:49:53 +0300

    Implement React router and create pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import GameContainer from "../components/GameContainer";
+import Main from "../components/Main";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <GameContainer />
+      <GameContainer />
+      <GameContainer />
+      <GameContainer />
+    </>
+  );
+};
+
+export default Home;
